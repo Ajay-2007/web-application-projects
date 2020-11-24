05165455bdf2d5d9cbc64e31f457efe812563be4
Ajay-2007 <iit2017040@iiita.ac.in>
2020-11-24 17:33:30 +0530

    added user, book and author models, run.py and pydantic_naming.py

---
+++ FastAPI/bookstoreAPI/models/user.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+import enum
+from fastapi import Query
+
+
+class Role(str, enum.Enum):
+    admin: str = "admin"
+    personel: str = "personel"
+
+
+class User(BaseModel):
+    name: str
+    password: str
+    mail: str = Query(None, regex="^([a-zA-Z0-9 \-\.]+)@([a-zA-Z0-9 \-\.]+)\.([a-zA-Z]{2,5})$")
+    role: Role
---
+++ FastAPI/pydantic_naming.py
@@ -0,0 +1,16 @@
+import datetime
+from typing import Dict, List, Tuple, Set
+from pydantic import BaseModel
+
+
+class Book(BaseModel):
+    name: str
+    price: float = 10.0
+    year: datetime.datetime
+
+
+book1 = {"name": "book1", "price": 11.0, "year": datetime.datetime.now()}
+
+book_object = Book(**book1)
+
+print(book_object.price)
