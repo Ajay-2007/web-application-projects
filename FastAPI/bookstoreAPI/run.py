a007aab6f1db39ef68e9a048bb500591db2b4a69
Ajay-2007 <iit2017040@iiita.ac.in>
2020-11-24 18:08:29 +0530

    added some advanced features of FastAPI

--- FastAPI/bookstoreAPI/run.py
+++ FastAPI/bookstoreAPI/run.py
@@ -1,14 +1,19 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Header, File
 from models.user import User
 from models.author import Author
 from models.book import Book
+from starlette.status import HTTP_201_CREATED
+from starlette.responses import Response
 
 app = FastAPI()
 
 
-@app.post("/user")
-async def post_user(user: User):
-    return {"request body": user}
+@app.post("/user", status_code=HTTP_201_CREATED)
+async def post_user(user: User, x_custom: str = Header("default")):
+    return {
+        "request body": user,
+        "request custom header": x_custom
+    }
 
 
 @app.get("/user")
@@ -18,17 +23,29 @@ async def get_user_validation(password: str):
     }
 
 
-@app.get("/book/{isbn}")
+@app.get("/book/{isbn}", response_model=Book, response_model_include=["name", "year"])
 async def get_book_with_isbn(isbn: str):
-    return {
-        "query changable parameter": isbn
+    author_dict = {
+        "name": "author1",
+        "book": ["book1", "book2"]
     }
+
+    author1 = Author(**author_dict)
+    book_dict = {
+        "isbn": "isbn1",
+        "name": "book1",
+        "year": 2019,
+        "author": author1
+    }
+    book1 = Book(**book_dict)
+
+    return book1
 
 
 @app.get("/author/{id}/book")
-async def get_authors_books(id: int, category: str, order: str = "asc", ):
+async def get_authors_books(idk: int, category: str, order: str = "asc", ):
     return {
-        "query changable parameter": order + category + str(id)
+        "query changable parameter": order + category + str(idk)
     }
 
 
@@ -48,44 +65,10 @@ async def post_user_and_author(user: User, author: Author, bookstore_name: str =
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@app.post("/user/photo")
+async def upload_user_photo(response: Response, profile_photo: bytes = File(...)):
+    response.headers["x-file-size"] = str(len(profile_photo))
+    response.set_cookie(key="cookie-api", value="test")
+    return {
+        "file size": len(profile_photo)
+    }
