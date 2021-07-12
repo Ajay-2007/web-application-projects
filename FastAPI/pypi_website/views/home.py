b294c3b8a81c739bf3716483b1d26205b109bc3c
Ajay-2007 <iit2017040@iiita.ac.in>
2021-07-12 16:12:27 +0530

    updated template files

--- FastAPI/pypi_website/views/home.py
+++ FastAPI/pypi_website/views/home.py
@@ -9,6 +9,14 @@ router = fastapi.APIRouter()
 @template()
 def index(user: str = 'anon'):
     return {
+        'package_count': 274_800,
+        'release_count': 2_234_847,
+        'user_count': 73_874,
+        'packages': [
+            {'id': 'fastapi', 'summary': "A great web framework"},
+            {'id': 'uvicorn', 'summary': "Your favorite ASGI server"},
+            {'id': 'httpx', 'summary': "Requests for an async world"},
+        ],
         'user_name': user
     }
 
